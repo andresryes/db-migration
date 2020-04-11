6b261f98bbc0d508c7560502e325096cd63fbec9
andresryes <enriquebolanos@ufm.edu>
2020-04-11 14:12:00 -0600

    edited routes

--- app/server.js
+++ app/server.js
@@ -59,19 +59,31 @@ app.get('/count', async (req, res) => {
 	insert into employees  values (1,'Steven','King','SKINGa','515.123.4567',to_date('17-JUN-03','DD-MON-RR'),'AD_PRES',24000,null,null,90);
 */
 app.post('/empleado', async (req, res) => {
+  let b = req.body
   if (process.env.NODE_DB === "ORACLE"){
-	let b = req.body
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'insert into employees values (:0, :1, :2, :3, :4, to_date(:5,\'DD-MON-RR\'), :6, :7, :8, :9, :10, :11)',
-      [b.employee_id, b.first_name, b.last_name, b.email, b.phone_number, b.hire_date,
-	  b.job_id, b.salary, b.comission_pct, b.manager_id, b.department_id],
-	  {autoCommit:true}
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'insert into employees values (:0, :1, :2, :3, :4, to_date(:5,\'DD-MON-RR\'), :6, :7, :8, :9, :10, :11)',
+        [b.employee_id, b.first_name, b.last_name, b.email, b.phone_number, b.hire_date,
+      b.job_id, b.salary, b.comission_pct, b.manager_id, b.department_id],
+      {autoCommit:true}
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(err)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
@@ -79,18 +91,30 @@ app.post('/empleado', async (req, res) => {
 });
 
 app.post('/puesto', async (req, res) => {
+  let b = req.body
   if (process.env.NODE_DB === "ORACLE"){
-	let b = req.body
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'insert into jobs values (:0, :1, :2, :3)',
-      [b.job_id, b.job_title, b.min_salary, b.max_salary],
-	  {autoCommit:true}
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'insert into jobs values (:0, :1, :2, :3)',
+        [b.job_id, b.job_title, b.min_salary, b.max_salary],
+      {autoCommit:true}
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(result)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
@@ -98,18 +122,30 @@ app.post('/puesto', async (req, res) => {
 });
 
 app.post('/region', async (req, res) => {
+  let b = req.body
   if (process.env.NODE_DB === "ORACLE"){
-	let b = req.body
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'insert into regions values (:0, :1)',
-      [b.region_id, b.region_name],
-	  {autoCommit:true}
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'insert into regions values (:0, :1)',
+        [b.region_id, b.region_name],
+      {autoCommit:true}
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(result)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
@@ -117,18 +153,30 @@ app.post('/region', async (req, res) => {
 });
 
 app.post('/pais', async (req, res) => {
+  let b = req.body
   if (process.env.NODE_DB === "ORACLE"){
-	let b = req.body
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'insert into countries values (:0, :1, :2)',
-      [b.country_id, b.country_name, b.region_id],
-	  {autoCommit:true}
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'insert into countries values (:0, :1, :2)',
+        [b.country_id, b.country_name, b.region_id],
+      {autoCommit:true}
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(result)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
@@ -136,18 +184,30 @@ app.post('/pais', async (req, res) => {
 });
 
 app.post('/location', async (req, res) => {
+  let b = req.body
   if (process.env.NODE_DB === "ORACLE"){
-	let b = req.body
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'insert into locations values (:0, :1, :2, :3, :4, :5)',
-      [b.location_id, b.street_address, b.postal_code, b.city, b.state_province, b.country_id],
-	  {autoCommit:true}
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'insert into locations values (:0, :1, :2, :3, :4, :5)',
+        [b.location_id, b.street_address, b.postal_code, b.city, b.state_province, b.country_id],
+      {autoCommit:true}
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(result)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
@@ -160,16 +220,28 @@ app.post('/location', async (req, res) => {
 */
 app.post('/empleado/:idEmpleado', async (req, res) => {
   if (process.env.NODE_DB === "ORACLE"){
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'update employees set salary = :salary, job_id = :job where employee_id = :id',
-      [req.body.salary, req.body.job_id, req.params.idEmpleado],
-	  {autoCommit:true}
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'update employees set salary = :salary, job_id = :job where employee_id = :id',
+        [req.body.salary, req.body.job_id, req.params.idEmpleado],
+      {autoCommit:true}
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(result)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
@@ -184,16 +256,28 @@ app.post('/empleado/:idEmpleado', async (req, res) => {
 */
 app.delete('/empleado/:idEmpleado', async (req, res) => {
   if (process.env.NODE_DB === "ORACLE"){
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'delete from employees where employee_id = :id',
-      [req.params.idEmpleado],
-	  {autoCommit:true}
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'delete from employees where employee_id = :id',
+        [req.params.idEmpleado],
+      {autoCommit:true}
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(result)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
@@ -203,15 +287,27 @@ app.delete('/empleado/:idEmpleado', async (req, res) => {
 /* Consultar empleado por ID */
 app.get('/empleado/:idEmpleado', async (req, res) => {
   if (process.env.NODE_DB === "ORACLE"){
-    let conn;
-    conn = await oracledb.getConnection(config);
-    const result =  await conn.execute(
-      'select * from employees where employee_id = :id',
-      [req.params.idEmpleado]
-    );
-    await conn.close();
-    console.log(result);
-    res.send(result);
+    try{
+      let conn;
+      conn = await oracledb.getConnection(config);
+      const result =  await conn.execute(
+        'select * from employees where employee_id = :id',
+        [req.params.idEmpleado]
+      );
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.send(result)
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   } 
   else{
     res.send("");
