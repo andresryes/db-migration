4572ae55f2de2ec748f23e43346380be4f72e872
andresryes <enriquebolanos@ufm.edu>
2020-04-11 17:57:05 -0600

    modify dbconfig

--- app/dbConfig.js
+++ app/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-    user          : process.env.NODE_ORACLEDB_USER,
-    password      : process.env.NODE_ORACLEDB_PASSWORD,
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING,
+    user          : process.env.NODE_DB_USER,
+    password      : process.env.NODE_DB_PASSWORD,
+    connectString : process.env.NODE_DB_CONNECTIONSTRING,
     poolMax: 2,
     poolMin: 2,
     poolIncrement: 0
